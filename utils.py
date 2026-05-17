ad2d84423ff2bcc93b125fa513ecba0ff27f0d6c
FranMorris <74791906+FranMorris@users.noreply.github.com>
2026-05-17 10:11:37 +0000

    customise lat-lon ranges; edit coarsening to be less specific

--- utils.py
+++ utils.py
@@ -44,10 +44,11 @@ def tb_from_olr(OLR):
 #    - relon = native grid longitude is 0-360, which is naff for East Atlantic. If True, relabel as -180-180
 #    - stream = which pp file from a given day to load. There are 4 main options, a--d, with c/d holding pressure-level data.
 #    - p = pressure range. If loading a/b streams don't need to do anything, applied only if stream=c/d
-# IMPORTANT: the function crops data to an Atlantic domain internally. Modify this if working elsewhere.
+#    - minlon/maxlon/minlat/maxlat - variables to define cropping to a particular region
+#    - coarsen - if false, do not interpolate to a coarser grid; if not false, can be set to the degree resolution to coarsen to
 ###################
 def load_file(
-    date, var_name, sim="n1280_GAL9", relon=True, stream="c", p=slice(None, None)
+    date, var_name, sim="n1280_GAL9", relon=True, stream="c", p=slice(None, None), minlon=-180, maxlon=180, minlat=-90, maxlat=90, coarsen_grid=False, coarsen_timestep=1
 ):
     date_str = "%04d%02d%02d" % (date.year, date.month, date.day)
     # data in 12 hour chunks, labelled 0 or 12, so get appropriate value.
@@ -61,7 +62,7 @@ def load_file(
                 ),
                 var_name,
             )[-1]
-        ).sel(latitude=slice(-5, 45))
+        ).sel(latitude=slice(minlat,maxlat))
     elif sim == "n2560_RAL3p3":
         ds = xr.DataArray.from_iris(
             iris.load(
@@ -70,7 +71,7 @@ def load_file(
                 ),
                 var_name,
             )[-1]
-        ).sel(latitude=slice(-5, 45))
+        ).sel(latitude=slice(minlat,maxlat))
     elif sim == "n2560_RAL3p3_tuned":
         ds = xr.DataArray.from_iris(
             iris.load(
@@ -79,7 +80,7 @@ def load_file(
                 ),
                 var_name,
             )[0]
-        ).sel(latitude=slice(-5, 45))
+        ).sel(latitude=slice(minlat,maxlat))
     elif sim == "n1280_10km-CoMA9":
         ds = xr.DataArray.from_iris(
             iris.load(
@@ -88,7 +89,7 @@ def load_file(
                 ),
                 var_name,
             )[0]
-        ).sel(latitude=slice(-5, 45))
+        ).sel(latitude=slice(minlat,maxlat))
     elif sim == "n1280_GAL9":
         ds = xr.DataArray.from_iris(
             iris.load(
@@ -97,7 +98,7 @@ def load_file(
                 ),
                 var_name,
             )[-1]
-        ).sel(latitude=slice(-5, 45))
+        ).sel(latitude=slice(minlat,maxlat))
 
     if stream == "c" or stream == "d":
         ds = ds.sel(pressure=p)
@@ -106,16 +107,17 @@ def load_file(
         ds = (
             ds.assign_coords(longitude=(((ds.longitude + 180) % 360) - 180))
             .sortby("longitude")
-            .sel(longitude=slice(-85, 55))
+            .sel(longitude=slice(minlon, maxlon))
         )
-    if (
-        args.coarsen is not None
-    ):  # GLOBAL variable. Use for mean state to speed things up significantly
-        # For vertical profile data - coarsen to 0.5 degrees and restrict to 6 hourly timesteps (same as TRACK)
-        lons, lats = np.arange(-85, 55.5, 0.5), np.arange(-5, 45.5, 0.5)
+    if coarsen_grid != False: 
+        # optional coarsening step; currently specified just for vertical profiles
+        # spatial coarsening defined by degree spacing specified by coarsen_grid 
+        # temporal coarsening defined by hour spacing specified by coarsen_timestep 
+        # TODO: change this to be area-weighted regridding probably
+        lons, lats = np.arange(minlon, maxlon, coarsen), np.arange(minlat, maxlat, coarsen_grid)
         if stream == "c" or stream == "d":
             ds = (
-                ds[ds.time.dt.hour.isin(np.arange(0, 24, 6))]
+                ds[ds.time.dt.hour.isin(np.arange(0, 24, coarsen_timestep))]
                 .interp(longitude=lons)
                 .interp(latitude=lats)
             )
